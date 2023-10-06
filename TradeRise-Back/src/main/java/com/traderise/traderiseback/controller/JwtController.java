5af27b81e62b4e3204f868cfccfbda226066f914
Yassine Jrad <yassine.jrad1@esprit.tn>
2023-10-06 13:44:43 +0100

    auth back

---
+++ TradeRise-Back/src/main/java/com/traderise/traderiseback/controller/JwtController.java
@@ -0,0 +1,24 @@
+package com.traderise.traderiseback.controller;
+
+import com.traderise.traderiseback.entity.JwtRequest;
+import com.traderise.traderiseback.entity.JwtResponse;
+import com.traderise.traderiseback.service.JwtService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class JwtController {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PostMapping({"/authenticate"})
+    public JwtResponse createJwtToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+        return jwtService.createJwtToken(jwtRequest);
+    }
+}
